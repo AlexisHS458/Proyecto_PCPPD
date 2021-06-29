cd125dd60e3489d74a2f93cae13bdd0111baaee0
Héctor Galván <hagalvangarcia@gmail.com>
2021-06-28 21:53:05 -0500

    Agregada paleta de colores
    Se agregó la paleta de colores con light y dark, omitiendo el normal porque las propiedades de Vuetify solo son dos.

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,4 +4,24 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme:{ //Paleta de colores elegida para el proyecto
+        themes: { 
+            light:{ 
+                primary: '#3e527e',
+                secondary: '#46606f',
+                error: '#d84a54',
+                info: '#d9ffff',
+                success: '#a1db45',
+                warning: '#ffbd45'
+            },
+            dark: {
+                primary: '#000029',
+                secondary: '#00101d',
+                error: '#6b0001',
+                info: '#75b4a9',
+                success: '#3b7900',
+                warning: '#c55d00'
+            }
+        }
+    }
 });
