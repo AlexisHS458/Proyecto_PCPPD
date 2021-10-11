ee002afed7bba43fd9b6ba28baf1e1e081dad5e4
Héctor Zamudio <47469444+zamudio1243@users.noreply.github.com>
2021-10-11 14:26:36 -0500

    :bug: Cambios en la conexión al endpoint

--- src/socketio.ts
+++ src/socketio.ts
@@ -3,6 +3,7 @@ import { io, Socket } from "socket.io-client";
 const endpoit = "http://pcppd.herokuapp.com/voiceChannel";
 let vcSocket: Socket;
 let vcUID: string;
+
 const voiceChannelSocket = (uid: string): Socket => {
   if (vcSocket && vcUID === uid) {
     return vcSocket;
@@ -16,20 +17,21 @@ const voiceChannelSocket = (uid: string): Socket => {
     return socket;
   }
 };
-export { voiceChannelSocket };
 
+let ccSocket: Socket;
+let ccUID: string;
 const endpoitCC = "http://pcppd.herokuapp.com/codeChannel";
 const codeChannelSocket = (uid: string): Socket => {
-  if (vcSocket && vcUID === uid) {
-    return vcSocket;
+  if (ccSocket && ccUID === uid) {
+    return ccSocket;
   } else {
     const socket = io(endpoitCC, {
       transports: ["websocket"],
       auth: { uid }
     });
-    vcSocket = socket;
-    vcUID = uid;
+    ccSocket = socket;
+    ccUID = uid;
     return socket;
   }
 };
-export { codeChannelSocket };
+export { codeChannelSocket, voiceChannelSocket};
