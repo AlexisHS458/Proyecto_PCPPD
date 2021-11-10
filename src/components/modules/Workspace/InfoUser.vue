851f615da2e2d732dbb3f6fb915ffdfd1560a6db
Alexis Herrera Saucedo <alexishs451@gmail.com>
2021-11-10 11:08:22 -0600

    :construction: Desconectar del canal de código

--- src/components/modules/Workspace/InfoUser.vue
+++ src/components/modules/Workspace/InfoUser.vue
@@ -1,22 +1,37 @@
 <template>
   <div>
-    <v-app-bar v-if="isConnected" color="primaryDark" dense class="toolbar">
-      <v-toolbar-title v-if="isConnectedStatus == 'Conectando'" class="text-color-connecting">
+    <v-app-bar
+      v-if="isConnected || iSConnectedCode"
+      color="primaryDark"
+      dense
+      class="toolbar"
+    >
+      <v-toolbar-title
+        v-if="isConnectedStatus == 'Conectando'"
+        class="text-color-connecting"
+      >
         {{ isConnectedStatus }}
       </v-toolbar-title>
-      <v-toolbar-title v-else-if="isConnectedStatus == 'Conectado'" class="text-color">
+      <v-toolbar-title
+        v-else-if="isConnectedStatus == 'Conectado'"
+        class="text-color"
+      >
         {{ isConnectedStatus }}
       </v-toolbar-title>
 
       <v-spacer></v-spacer>
       <v-btn
         icon
-        @click="disconnect('http://soundbible.com/mp3/Elevator Ding-SoundBible.com-685385892.mp3')"
+        @click="
+          disconnect(
+            'http://soundbible.com/mp3/Elevator Ding-SoundBible.com-685385892.mp3'
+          )
+        "
         v-if="isConnected"
       >
         <v-icon color="errorLight">mdi-phone-remove</v-icon>
       </v-btn>
-      <v-btn icon>
+      <v-btn icon @click="disconnectCode" v-if="iSConnectedCode">
         <v-icon color="errorLight">mdi-xml</v-icon>
       </v-btn>
     </v-app-bar>
@@ -27,7 +42,7 @@
             {{ currentUser.nombre + " " + currentUser.apellido }}
           </v-toolbar-title>
         </template>
-        <span>{{ currentUser.nombre + " " + currentUser.apellido }}</span>
+        <span>{{  isConnected + " " + iSConnectedCode}}</span>
       </v-tooltip>
       <v-spacer></v-spacer>
       <v-btn icon @click="toggleMicrophone">
@@ -38,12 +53,6 @@
         <v-icon v-if="isListening" color="success">mdi-headphones</v-icon>
         <v-icon v-else color="error">mdi-headphones-off</v-icon>
       </v-btn>
-      <!--  <v-btn icon @click="disconnect" v-if="isConnected">
-        <v-icon color="errorLight">mdi-phone-remove</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon color="errorLight">mdi-xml</v-icon>
-      </v-btn> -->
     </v-app-bar>
   </div>
 </template>
@@ -55,13 +64,14 @@ import { namespace } from "vuex-class";
 import { User } from "@/models/user";
 import { Prop } from "vue-property-decorator";
 import VoiceService from "@/services/voice_channel.service";
+import CodeService from "@/services/code_channel.service";
 import { VoiceState } from "@/utils/voiceState";
 const User = namespace("UserModule");
 const StatusVoice = namespace("VoiceChannelModule");
 @Component
 export default class UserInfo extends Vue {
   @Prop({
-    required: true
+    required: true,
   })
   public currentUser!: User;
 
@@ -72,6 +82,8 @@ export default class UserInfo extends Vue {
   public isTalk = true;
   public isListening = true;
   public isConnected = false;
+  public iSConnectedCode = false;
+ // currentUser.nombre + " " + currentUser.apellido
 
   toggleMicrophone() {
     this.isTalk = !this.isTalk;
@@ -90,9 +102,16 @@ export default class UserInfo extends Vue {
     }
   }
 
+  disconnectCode() {
+    CodeService.leaveCodeChannel(this.currentUser.uid!);
+  }
+
   mounted() {
-    VoiceService.userStatus(this.currentUser.uid!, isConnected => {
+    VoiceService.userStatus(this.currentUser.uid!, (isConnected) => {
       this.isConnected = !!isConnected;
+    });
+    CodeService.userStatus(this.currentUser.uid!, (isConnected) => {
+      this.iSConnectedCode = !!isConnected;
     });
   }
 }
