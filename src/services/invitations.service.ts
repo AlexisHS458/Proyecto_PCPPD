9abfb47ef38fa07f49baf78c9581413312b59d7d
Héctor Zamudio <47469444+zamudio1243@users.noreply.github.com>
2021-11-29 19:09:20 -0600

    :construction: 	Limitación de almacenamiento

--- src/services/invitations.service.ts
+++ src/services/invitations.service.ts
@@ -1,87 +1,105 @@
 import { Invitation } from "@/models/invitation";
 import { User } from "@/models/user";
 import { Collection } from "@/utils/collections";
-import { db, FieldValue} from "@/utils/firebase";
+import { db, FieldValue } from "@/utils/firebase";
 import UserService from "@/services/user.service";
+import ChannelsService from "./channels.service";
+import PermissionsService from "./permissions.service";
 
 /**
  * Servicio de invitaciones de espacios de trabajo.
  */
-class InvitationsService{
+class InvitationsService {
+  /**
+   * Enviar una invitación al espacio de trabajo
+   * @param IdUsuario ID del usuario que recibirá la invitación
+   * @param invitation Invitación al espacio de trabajo
+   */
+  async sendInivitation(invitation: Invitation): Promise<void> {
+    await db
+      .collection(Collection.USERS)
+      .doc(invitation.idUsuarioInvitado)
+      .collection(Collection.INVITATIONS)
+      .add(invitation);
+  }
 
-    /**
-     * Enviar una invitación al espacio de trabajo 
-     * @param IdUsuario ID del usuario que recibirá la invitación
-     * @param invitation Invitación al espacio de trabajo
-     */
-    async sendInivitation(invitation: Invitation): Promise<void>{
-        await db.collection(Collection.USERS).doc(invitation.idUsuarioInvitado)
-        .collection(Collection.INVITATIONS).add(invitation);
-    }
+  /**
+   * Obtiene las invitaciones de un usuario
+   * @param uidUsuario uidUsario a consultar sus invitaciones
+   * @param onSnapshot suscripción de nuevas invitaciones
+   */
+  getInvitations(uidUsuario: string, onSnapshot: (invitations: Invitation[]) => void): void {
+    db.collection(Collection.USERS)
+      .doc(uidUsuario)
+      .collection(Collection.INVITATIONS)
+      .onSnapshot(snapshot => {
+        onSnapshot(
+          snapshot.docs.map<Invitation>(doc => {
+            const invitation = {
+              ...doc.data(),
+              uid: doc.id
+            };
+            return <Invitation>invitation;
+          })
+        );
+      });
+  }
 
-    /**
-     * Obtiene las invitaciones de un usuario
-     * @param uidUsuario uidUsario a consultar sus invitaciones
-     * @param onSnapshot suscripción de nuevas invitaciones
-     */
-    getInvitations(
-        uidUsuario: string,
-        onSnapshot: (invitations: Invitation[]) => void
-    ): void {
-        db.collection(Collection.USERS).doc(uidUsuario)
-        .collection(Collection.INVITATIONS).onSnapshot(snapshot => {
-            onSnapshot(
-                snapshot.docs.map<Invitation>(doc => {
-                    const invitation = {
-                        ...doc.data(),
-                        uid: doc.id
-                    }
-                    return <Invitation>invitation;
-                })
-            );
-        });
-    }
+  /**
+   * Recupera los nombres de los usuarios
+   * @param name nombre del usuario a buscar
+   */
+  getUserNames(name: string, onSnapshot: (user: User[]) => void): void {
+    db.collection(Collection.USERS)
+      .where("nombre", "array-contains", name)
+      .onSnapshot(snapshot => {
+        onSnapshot(
+          snapshot.docs.map<User>(doc => {
+            const user = {
+              ...doc.data(),
+              uid: doc.id
+            };
+            return <User>user;
+          })
+        );
+      });
+  }
 
-    /**
-    * Recupera los nombres de los usuarios
-    * @param name nombre del usuario a buscar
-    */
-    getUserNames(name: string, onSnapshot: (user: User[]) => void): void {
-        db.collection(Collection.USERS).where("nombre", "array-contains", name).onSnapshot(snapshot => {
-            onSnapshot(
-                snapshot.docs.map<User>(doc => {
-                    const user = {
-                        ...doc.data(),
-                        uid: doc.id
-                    };
-                    return <User>user;
-                })
-            );
-        });
-    }
+  /**
+   * Acepta la invitación a un espacio de trabajo y se agrega el usuario al
+   * espacio de trabajo
+   * @param invitation
+   */
+  async acceptInvitation(invitation: Invitation): Promise<void> {
+    await db
+      .collection(Collection.WORK_SPACE)
+      .doc(invitation.idEspacioTrabajo)
+      .update({
+        usuarios: FieldValue.arrayUnion(invitation.idUsuarioInvitado)
+      });
+    await UserService.updateUserWorkspaceCollab(invitation.idUsuarioInvitado, true);
+    const channelId = await ChannelsService.getFirstTextChannelID(invitation.idEspacioTrabajo);
+    
+    await PermissionsService.AddPermission(
+      invitation.idEspacioTrabajo,
+      channelId,
+      invitation.idUsuarioInvitado
+    );
+    return await this.deleteInvitation(invitation);
+  }
 
-    /**
-     * Acepta la invitación a un espacio de trabajo y se agrega el usuario al
-     * espacio de trabajo
-     * @param invitation 
-     */
-    async acceptInvitation(invitation: Invitation): Promise<void>{
-        await db.collection(Collection.WORK_SPACE).doc(invitation.idEspacioTrabajo)
-        .update({
-            'usuarios': FieldValue.arrayUnion(invitation.idUsuarioInvitado)
-        });
-        await UserService.updateUserWorkspaceCollab(invitation.idUsuarioInvitado, true);
-        return await this.deleteInvitation(invitation);
-    }
-
-    /**
-     * Elimina la invitación al espacio de trabajo, rechazandola.
-     * @param invitation invitacion a eliminar
-     */
-    async deleteInvitation(invitation: Invitation): Promise<void>{
-        return await db.collection(Collection.USERS).doc(invitation.idUsuarioInvitado)
-        .collection(Collection.INVITATIONS).doc(invitation.uid).delete();
-    }
+  /**
+   * Elimina la invitación al espacio de trabajo, rechazandola.
+   * @param invitation invitacion a eliminar
+   */
+  async deleteInvitation(invitation: Invitation): Promise<void> {
+    return await db
+      .collection(Collection.USERS)
+      .doc(invitation.idUsuarioInvitado)
+      .collection(Collection.INVITATIONS)
+      .doc(invitation.uid)
+      .delete();
+  }
 }
 
 export default new InvitationsService();
--- src/services/message.service.ts
+++ src/services/message.service.ts
@@ -2,11 +2,13 @@ import { db } from "@/utils/firebase";
 import { Message } from "@/models/message";
 import { Collection } from "@/utils/collections";
 import { storage } from "@/utils/firebase";
+import WorkspaceService from "./work_space.service";
 
 /**
  * Conexión a servicios de información de los espacios de trabajo.
  */
 class MessageService {
+  public MAX_SIZE = 1000000; // 150000000
   /**
    * Agrega un nuevo mensaje
    * @param workspaceID ID del espacio de trabajo correspondiente
@@ -52,38 +54,42 @@ class MessageService {
     message: Message,
     file: File
   ): Promise<Message> {
-    const messageRef = (
-      await db
-        .collection(Collection.WORK_SPACE)
-        .doc(workspaceID)
-        .collection(Collection.TEXT_CHANNEL)
-        .doc(textChannelID)
-        .collection(Collection.MESSAGES)
-        .add(message)
-    )
-      .get()
-      .then(async data => {
-        const message = <Message>(data).data();
-        const storageRef = storage.ref();
-        const fileRef = storageRef.child(data.id);
-        await fileRef.put(file);
-        const metaData = await fileRef.getMetadata();
-        const fileURL = await fileRef.getDownloadURL();
+    const workspace = await WorkspaceService.getWorkspaceInfo(workspaceID);
 
-        message.nombreArchivo = file.name;
-        message.contentType = metaData.contentType;
-        message.contenido = fileURL;
-        message.uid = data.id
+    if (workspace.almacenamiento + file.size < this.MAX_SIZE) {
+      const messageRef = (
+        await db
+          .collection(Collection.WORK_SPACE)
+          .doc(workspaceID)
+          .collection(Collection.TEXT_CHANNEL)
+          .doc(textChannelID)
+          .collection(Collection.MESSAGES)
+          .add(message)
+      )
+        .get()
+        .then(async data => {
+          const message = <Message>data.data();
 
-       
-        
+          const storageRef = storage.ref();
+          const fileRef = storageRef.child(data.id);
+          await fileRef.put(file);
+          const metaData = await fileRef.getMetadata();
+          const fileURL = await fileRef.getDownloadURL();
 
-        await this.editMessage(workspaceID, textChannelID, message);
+          message.nombreArchivo = file.name;
+          message.contentType = metaData.contentType;
+          message.contenido = fileURL;
+          message.uid = data.id;
 
-        return data;
-      });
+          await this.editMessage(workspaceID, textChannelID, message);
+          await WorkspaceService.updateWorkspaceStorage(workspaceID, true, file.size);
 
-    return <Message>(await messageRef).data();
+          return data;
+        });
+      return <Message>(await messageRef).data();
+    } else {
+      throw new Error("No hay mas almacenamiento");
+    }
   }
 
   /**
@@ -93,8 +99,6 @@ class MessageService {
    * @param message Mensaje a enviar al canal de texto
    */
   async editMessage(workspaceID: string, textChannelID: string, message: Message): Promise<void> {
-
-
     await db
       .collection(Collection.WORK_SPACE)
       .doc(workspaceID)
@@ -111,11 +115,7 @@ class MessageService {
    * @param textChannelID ID del canal de texto
    * @param messageID ID del documento a eliminar
    */
-  async deleteMessage(
-    workspaceID: string,
-    textChannelID: string,
-    message: Message
-  ): Promise<void> {
+  async deleteMessage(workspaceID: string, textChannelID: string, message: Message): Promise<void> {
     await db
       .collection(Collection.WORK_SPACE)
       .doc(workspaceID)
@@ -125,10 +125,10 @@ class MessageService {
       .doc(message.uid)
       .delete();
 
-    if(message.isFile){
+    if (message.isFile) {
       const storageRef = storage.ref();
-      const fileRef = storageRef.child(message.uid!)
-      await fileRef.delete()
+      const fileRef = storageRef.child(message.uid!);
+      await fileRef.delete();
     }
   }
 
