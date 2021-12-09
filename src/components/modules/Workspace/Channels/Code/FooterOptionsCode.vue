c2b379f315c275c25359fd7114b4e6374f4faa23
Héctor Zamudio <47469444+zamudio1243@users.noreply.github.com>
2021-12-08 23:59:50 -0600

    :lipstick: Ahora no se muestra el stdin a los navigator

--- src/components/modules/Workspace/Channels/Code/FooterOptionsCode.vue
+++ src/components/modules/Workspace/Channels/Code/FooterOptionsCode.vue
@@ -54,7 +54,14 @@
           Argumentos
         </v-btn> -->
         <v-divider vertical></v-divider>
-        <v-btn depressed text color="white" class="mr-2 text-capitalize" @click="closeInputStdin">
+        <v-btn
+          v-if="currentUser.uid == driverUID"
+          depressed
+          text
+          color="white"
+          class="mr-2 text-capitalize"
+          @click="closeInputStdin"
+        >
           Stdin
         </v-btn>
         <v-spacer></v-spacer>
@@ -64,10 +71,12 @@
   </v-footer>
 </template>
 <script lang="ts">
+import { User } from "@/models/user";
 import { Position } from "monaco-editor";
 import { Component, Prop, Vue } from "vue-property-decorator";
 import { namespace } from "vuex-class";
 const CodeChannel = namespace("CodeChannelModule");
+const User = namespace("UserModule");
 @Component
 export default class EditCode extends Vue {
   @Prop({
@@ -89,6 +98,15 @@ export default class EditCode extends Vue {
 
   @CodeChannel.State("codeFilePath")
   private codeFilePath!: string;
+
+  @CodeChannel.State("driverUID")
+  private driverUID!: string | undefined;
+
+  /**
+   * Estado obtenido del @module User
+   */
+  @User.State("user")
+  private currentUser!: User;
 
   public showStdin = false;
   public showArgs = false;
